fee0af11c37935a543954f93bc963f6df1e9931d
jaeho <jaeho.jeong1228@greenlabs.co.kr>
2022-10-17 18:16:42 +0900

    feat: search by text, it works anyway... the cache mechanism is amazing

--- src/pages/RepoList.tsx
+++ src/pages/RepoList.tsx
@@ -1,5 +1,5 @@
 import { graphql } from 'babel-plugin-relay/macro'
-import React, { useEffect } from 'react'
+import React, { SyntheticEvent, useEffect, useState } from 'react'
 import { loadQuery, useClientQuery } from 'react-relay'
 import { PageInfo } from 'relay-runtime'
 import RelayEnv from '../RelayEnv'
@@ -28,8 +28,8 @@ interface RepoListSearchQuery {
 }
 
 const RepoListQuery = graphql`
-    query RepoListSearchQuery {
-        search(query: "is:public", type:REPOSITORY, first:20) {
+    query RepoListSearchQuery($query: String!) {
+        search(query: $query, type:REPOSITORY, first:20) {
             repositoryCount
             pageInfo {
                 startCursor
@@ -54,10 +54,11 @@ const RepoListQuery = graphql`
 `
 
 export function RepoList() {
-  const data = useClientQuery(RepoListQuery, {}) as RepoListSearchQuery
+  const [repoName, setRepoName] = useState('')
+  const data = useClientQuery(RepoListQuery, { query: repoName }) as RepoListSearchQuery
 
   useEffect(function () {
-    loadQuery(RelayEnv, RepoListQuery, {})
+    loadQuery(RelayEnv, RepoListQuery, { query: repoName || 'is:public' })
   }, [])
 
   function renderRepository() {
@@ -71,12 +72,20 @@ export function RepoList() {
     })
   }
 
+  function onRepoNameChange(e: SyntheticEvent<HTMLInputElement>) {
+    setRepoName((e.target as HTMLInputElement).value)
+  }
+
+  function search() {
+    loadQuery(RelayEnv, RepoListQuery, { query: repoName || 'is:public' })
+  }
+
   return (
     <div className="max-w-screen-md mx-auto px-4">
       <div>Hey~ I ❤️ TypeScript.</div>
       <div>
-        <input type="text" className="border border-1 p-1 rounded w-1/2" />
-        <button type="button" className="border border-1 p-1 ml-2 rounded">Search</button>
+        <input type="text" className="border border-1 p-1 rounded w-1/2" onChange={onRepoNameChange} value={repoName} />
+        <button type="button" className="border border-1 p-1 ml-2 rounded" onClick={search}>Search</button>
       </div>
       <ul className="text-left">
         {renderRepository()}
