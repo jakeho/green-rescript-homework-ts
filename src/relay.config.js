b85c1465377cdb100fa102f9262b7c6b4e801f14
jaeho <jaeho.jeong1228@greenlabs.co.kr>
2022-10-17 13:19:38 +0900

    chore: move the config for relay into the package.json

--- src/relay.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  src: './src',
-  language: 'typescript', // "javascript" | "typescript" | "flow"
-  schema: './schema.graphql',
-  exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**']
-};
